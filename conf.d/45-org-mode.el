a69b28f6fbabcd8401d112eafb121acb8505a787
Jianing Yang <jianingy.yang@gmail.com>
2018-09-06 13:47:21 +0800

    optimize org-agenda

--- conf.d/45-org-mode.el
+++ conf.d/45-org-mode.el
@@ -1,24 +1,33 @@
 (use-package org
   :ensure t
-  :init (setq
-          org-todo-keywords
-          '((sequence "TODO(t)" "WORKING" "SUSPENDED" "|"
-                      "DONE(d)" "ABANDONED(a)")
-            (sequence "CHECK(c)" "|" "VERIFIED(v)"))
-          org-agenda-prefix-format
-          '((agenda . " %?-12t% s ")
-            (todo . " %-12:s ● ")
-            (tags . " %i %-12:c")
-            (search . " %i %-12:c"))
-          org-priority-faces
-          '((65 :foreground "white" :background "red")
-            (66 :foreground "orange")
-            (67 . "royal blue"))
-          org-todo-keyword-faces
-          '(("CHECK" :foreground "orange")
-            ("WORKING" :foreground "white" :background "firebrick")
-            ("SUSPEND" :foreground "firebrick")
-            ("VERIFIED" :foreground "lime green" :weight normal)))
+  :init
+  (defun nby/org-agenda-breadcrumb ()
+    (concat
+     "["
+     (mapconcat 'identity
+                (mapcar (lambda (x) (format "%s" x))
+                        (org-get-outline-path))
+                " ≫ ")
+     "]"))
+  (setq
+   org-todo-keywords
+   '((sequence "TODO(t)" "WORKING" "SUSPENDED" "|"
+               "DONE(d)" "ABANDONED(a)")
+     (sequence "CHECK(c)" "|" "VERIFIED(v)"))
+   org-agenda-prefix-format
+   '((agenda . " %(nby/org-agenda-breadcrumb) %?-12t% s ")
+     (todo . "%(nby/org-agenda-breadcrumb) %i")
+     (tags . " %i %-12:c")
+     (search . " %i %-12:c"))
+   org-priority-faces
+   '((65 :foreground "white" :background "red")
+     (66 :foreground "orange")
+     (67 . "royal blue"))
+   org-todo-keyword-faces
+   '(("CHECK" :foreground "orange")
+     ("WORKING" :foreground "white" :background "firebrick")
+     ("SUSPEND" :foreground "firebrick")
+     ("VERIFIED" :foreground "lime green" :weight normal)))
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c c" . org-capture)))
