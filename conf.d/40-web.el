3c6f0a12d537d7083f363b7f07ed4028af29878e
Jianing Yang <jianingy.yang@gmail.com>
2018-11-08 22:40:29 +0800

    update web dev

--- conf.d/40-web.el
+++ conf.d/40-web.el
@@ -3,16 +3,22 @@
 ;;; Code:
 
 (use-package tern
+  :disabled
   :ensure t
   :after js2-mode
   :hook ((js2-mode . tern-mode))
-  :init (setq js-indent-level 2
-              js2-strict-missing-semi-warning nil
-              js2-missing-semi-one-line-override nil)
   :config
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   (flycheck-add-mode 'javascript-eslint 'js-mode)
   (flycheck-add-mode 'javascript-eslint 'js2-mode))
+
+(use-package company-tern
+  :disabled
+  :ensure t
+  :hook ((js2-mode . company-mode)
+         (js-mode . company-mode))
+  :config
+  (add-to-list 'company-backends 'company-tern))
 
 (use-package web-mode
   :mode ("\\.html\\'" . web-mode)
@@ -25,20 +31,31 @@
 (use-package vue-mode
   :mode ("\\.vue\\'" . vue-mode)
   :ensure t
+  :init (setq js-indent-level 2
+              js2-strict-missing-semi-warning nil
+              js2-missing-semi-one-line-override nil)
   :config
   (setq mmm-submode-decoration-level 0))
-
 
 (use-package lsp-vue
   :after (lsp-mode)
   :hook ((vue-mode . lsp-vue-mmm-enable))
   :ensure t)
 
-(use-package company-tern
+(use-package lsp-javascript-typescript
+  :after (lsp-mode)
   :ensure t
-  :hook ((js2-mode . company-mode)
-         (js-mode . company-mode))
-  :config
-  (add-to-list 'company-backends 'company-tern))
+  :init (setq js-indent-level 2
+              js2-strict-missing-semi-warning nil
+              js2-missing-semi-one-line-override nil)
+  :hook ((js2-mode . lsp-javascript-typescript-enable)))
+
+(use-package xref-js2
+  :after (js2-mode)
+  :ensure t)
+
+(use-package js2-refactor
+  :after (js2-mode)
+  :ensure t)
 
 ;;; ends here
