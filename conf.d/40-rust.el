f90c733cfd075492c6b8f7f84224bb6532cdde6b
Jianing Yang <jianingy.yang@gmail.com>
2019-04-27 20:35:51 +0800

    remove rust-lsp use lsp-mode

--- conf.d/40-rust.el
+++ conf.d/40-rust.el
@@ -5,16 +5,8 @@
 
 (use-package rust-mode
   :ensure
+  :hook (rust-mode . lsp)
   :mode ("\\.rs\\'" . rust-mode))
-
-(use-package lsp-rust
-  :ensure
-  :after lsp-mode
-  :hook (rust-mode . nby/setup-rust-mode)
-  :init
-  (defun nby/setup-rust-mode ()
-    (column-enforce-n 100)
-    (lsp)))
 
 (use-package rust-playground :ensure)
 
