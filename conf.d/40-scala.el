56ecfefd833f4184b8ad88d9494b215debee8f03
Jianing Yang <jianingy.yang@gmail.com>
2019-05-11 22:34:50 +0800

    add sbt-mode

--- conf.d/40-scala.el
+++ conf.d/40-scala.el
@@ -1,5 +1,7 @@
 (use-package scala-mode
   :ensure
+  :bind (:map scala-mode-map
+              ("C-c C-c" . run-scala))
   :interpreter
   ("scala" . scala-mode))
 
@@ -7,3 +9,13 @@
 (use-package lsp-scala
   :ensure
   :after (lsp scala-mode))
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
