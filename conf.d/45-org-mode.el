32ac3e58dda0749281a8e3834107d97802bdcdde
Jianing Yang <jianingy.yang@gmail.com>
2019-06-13 21:05:02 +0800

    ui tuning

--- conf.d/45-org-mode.el
+++ conf.d/45-org-mode.el
@@ -1,11 +1,11 @@
 (use-package org
   :ensure
   :custom-face
-  ;; (org-document-title ((t (:inherit nil :font "fontset-document" :fontset "fontset-document" :height 1.4 ))))
-  ;; (org-level-1 ((t (:inherit nil :font "fontset-document" :fontset "fontset-document" :height 1.3 ))))
-  ;; (org-level-2 ((t (:inherit nil :font "fontset-document" :fontset "fontset-document" :height 1.1))))
-  ;; (org-level-3 ((t (:weight bold :height 1.0))))
-  ;; (org-level-4 ((t (:height 1.0))))
+  (org-document-title ((t (:inherit nil :height 1.0 ))))
+  (org-level-1 ((t (:inherit nil :height 1.0 ))))
+  (org-level-2 ((t (:inherit nil :height 1.0))))
+  (org-level-3 ((t (:weight bold :height 1.0))))
+  (org-level-4 ((t (:height 1.0))))
   :init
   (defun nby/org-agenda-breadcrumb ()
     (concat
--- conf.d/95-workaround.el
+++ conf.d/95-workaround.el
@@ -10,29 +10,13 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(when (eq system-type 'gnu/linux)
-  ;; double bold font looks ugly on linux, so disable bold for all fonts.
-  (mapc #'(lambda (x) (set-face-attribute x nil :weight 'normal)) (face-list)))
+;; (when (eq system-type 'gnu/linux)
+;;   ;; double bold font looks ugly on linux, so disable bold for all fonts.
+;;   (mapc #'(lambda (x) (set-face-attribute x nil :weight 'normal)) (face-list)))
 
 (add-hook 'find-file-hook
           (lambda ()
             (when (file-remote-p default-directory)
               (setq-local projectile-mode-line "Projectile"))))
 
-;;; 下面一段是 zwz 的, 作者声明只适应 org-mode 8.0 以及以上版本
-(defun clear-single-linebreak-in-cjk-string (string)
-  "clear single line-break between cjk characters that is usually soft line-breaks"
-  (let* ((regexp "\\([\u4E00-\u9FA5]\\)\n\\([\u4E00-\u9FA5]\\)")
-         (start (string-match regexp string)))
-    (while start
-      (setq string (replace-match "\\1\\2" nil nil string)
-            start (string-match regexp string start))))
-  string)
-
-;; (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
-;;   (when (org-export-derived-backend-p backend 'html)
-;;     (clear-single-linebreak-in-cjk-string string)))
-
-;; (add-to-list 'org-export-filter-final-output-functions
-;;              'ox-html-clear-single-linebreak-for-cjk)
 ;;; ends here
